0f663ee0ffa9818f1ae0d54134080d5b8b80b961
xikai <xikai@kth.se>
2024-11-09 00:24:12 +0100

    hw2

--- hw/ex1/hw2_ex1_windows.cu
+++ hw/ex1/hw2_ex1_windows.cu
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <math.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-
+#include <Windows.h>
 
 #define DataType double
 
-__global__ void vecAdd(DataType* in1, DataType* in2, DataType* out, int len) {
+__global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len)
+{
 	//@@ Insert code to implement vector addition here
 	int i = threadIdx.x + blockDim.x * blockIdx.x;
 	if (i < len)
 		out[i] = in1[i] + in2[i];
 }
 
-//@@ Insert code to implement timer start
-
-//@@ Insert code to implement timer stop
-
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
 	int inputLength;
-	DataType* hostInput1;
-	DataType* hostInput2;
-	DataType* hostOutput;
-	DataType* resultRef;
-	DataType* deviceInput1;
-	DataType* deviceInput2;
-	DataType* deviceOutput;
+	DataType *hostInput1;
+	DataType *hostInput2;
+	DataType *hostOutput;
+	DataType *resultRef;
+	DataType *deviceInput1;
+	DataType *deviceInput2;
+	DataType *deviceOutput;
+	// Timer
+	LARGE_INTEGER frequency;
+	LARGE_INTEGER start, end;
+	double elapsedTime;
+	QueryPerformanceFrequency(&frequency);
 
 	//@@ Insert code below to read in inputLength from args
 	inputLength = atoi(argv[1]);
@@ -36,10 +38,10 @@ int main(int argc, char** argv) {
 
 	//@@ Insert code below to allocate Host memory for input and output
 	size_t size = inputLength * sizeof(DataType);
-	hostInput1 = (DataType*)malloc(size);
-	hostInput2 = (DataType*)malloc(size);
-	hostOutput = (DataType*)malloc(size);
-    resultRef =  (DataType*)malloc(size);
+	hostInput1 = (DataType *)malloc(size);
+	hostInput2 = (DataType *)malloc(size);
+	hostOutput = (DataType *)malloc(size);
+	resultRef = (DataType *)malloc(size);
 
 	//@@ Insert code below to initialize hostInput1 and hostInput2 to random numbers, and create reference result in CPU
 	for (int i = 0; i < inputLength; ++i)
@@ -55,31 +57,41 @@ int main(int argc, char** argv) {
 	cudaMalloc(&deviceInput2, size);
 	cudaMalloc(&deviceOutput, size);
 
-
-
-
 	//@@ Insert code to below to Copy memory to the GPU here
+	QueryPerformanceCounter(&start);
 	cudaMemcpy(deviceInput1, hostInput1, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(deviceInput2, hostInput2, size, cudaMemcpyHostToDevice);
+	QueryPerformanceCounter(&end);
+	elapsedTime = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+	printf("data copy from host to device: %f seconds\n", elapsedTime);
 
 	//@@ Initialize the 1D grid and block dimensions here
 	int threads_per_block = 256;
 	int number_of_blocks = (inputLength + threads_per_block - 1) / threads_per_block;
 
 	//@@ Launch the GPU Kernel here
-	vecAdd << <number_of_blocks, threads_per_block >> > (deviceInput1, deviceInput2, deviceOutput, inputLength);
-   
+	QueryPerformanceCounter(&start);
+	vecAdd<<<number_of_blocks, threads_per_block>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
+	QueryPerformanceCounter(&end);
+	elapsedTime = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+	printf("CUDA kernel: %f seconds\n", elapsedTime);
+
 	//@@ Copy the GPU memory back to the CPU here
+	QueryPerformanceCounter(&start);
 	cudaMemcpy(hostOutput, deviceOutput, size, cudaMemcpyDeviceToHost);
+	QueryPerformanceCounter(&end);
+	elapsedTime = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+	printf("data copy from device to host: %f seconds\n", elapsedTime);
 
 	//@@ Insert code below to compare the output with the reference
 	DataType max_error = 0;
-	for (int i = 0; i < inputLength; ++i) {
-		if (fabs(hostOutput[i] - resultRef[i]) > max_error) 
+	for (int i = 0; i < inputLength; ++i)
+	{
+		if (fabs(hostOutput[i] - resultRef[i]) > max_error)
 			max_error = fabs(hostOutput[i] - resultRef[i]);
 	}
 
-	if(max_error > 1e-5)
+	if (max_error > 1e-5)
 		printf("Problem! The Max Error of %.5f is NOT within acceptable bounds.\n", max_error);
 	else
 		printf("The Max Error of %.5f is within acceptable bounds.\n", max_error);
@@ -94,4 +106,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
